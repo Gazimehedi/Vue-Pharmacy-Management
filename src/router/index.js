714264686d19a398212f29a3e6a6a1f32e2132d7
Gazimehedi <gazimehedihasan512@gmail.com>
2022-10-01 18:50:45 +0600

    dashboard design done

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,26 @@
 import { createRouter, createWebHistory } from "vue-router";
 import DashboardPage from "../views/Dashboard.vue";
 import LoginPage from "../views/Login.vue";
+const Overview = () => import('../views/dashboard/Overview.vue');
+const Drugs = () => import('../views/dashboard/Drugs.vue');
+const Vendors = () => import('../views/dashboard/Vendors.vue');
+const SellingHistory = () => import('../views/dashboard/SellingHistory.vue');
+const Setting = () => import('../views/dashboard/Setting.vue');
 
 const routes = [
     {path:"/", component:LoginPage},
-    {path:"/dashboard", component:DashboardPage}
+    {
+        path:"/dashboard", 
+        component:DashboardPage,
+        redirect:"/dashboard/overview",
+        children:[
+            {path:"overview",component:Overview},
+            {path:"drugs",component:Drugs},
+            {path:"vendors",component:Vendors},
+            {path:"selling-history",component:SellingHistory},
+            {path:"setting",component:Setting},
+        ]
+    }
 ];
 
 const router = createRouter({
